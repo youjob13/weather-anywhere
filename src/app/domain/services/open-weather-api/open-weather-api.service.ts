3a5f23ec1582fc48af520d84e68ee2b9203f0416
Danil Rodionov <Danil_Rodionov@epam.com>
2025-01-11 16:03:03 +0400

    debug

--- src/app/domain/services/open-weather-api/open-weather-api.service.ts
+++ src/app/domain/services/open-weather-api/open-weather-api.service.ts
@@ -17,6 +17,7 @@ export class OpenWeatherAPIService {
     city: string,
     limit = 1
   ): Observable<IGeolocationRaw | null> {
+    console.log(this.openWeatherEndpoint);
     const url = `${this.openWeatherEndpoint}/geo/1.0/direct?q=${city}&limit=${limit}&appid=${this.apiKey}`;
     return this.httpClient
       .get<IGeolocationRaw[]>(url)
